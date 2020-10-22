2ccaf891037fc3cfddb9885e2a524f6596ff1e24
hoyama <ghettomoon00@gmail.com>
2020-10-23 00:51:35 +0900

    first mommit

---
+++ server/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('comments/freeword/{freeword2}/', 'GreetingController@fw');
+Route::get('comments/random/', 'GreetingController@rand');
+Route::get('comments/{greeting}/', 'GreetingController@hello');
+
+
+
+
+
+
+
